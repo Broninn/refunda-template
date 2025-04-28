f6d634a46b29736c6f43e1bed2c5a2c8332a1a21
broninn <bruno-hs@outlook.com>
2025-04-28 15:26:38 -0300

    Criando o elemento para adicionar na lista

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // Selecionar os elementos do formulário
+const form = document.querySelector("form");
 const inputAmount = document.getElementById("amount");
+const expense = document.getElementById("expense");
+const category = document.getElementById("category");
+
+//Seleciona os elementos da lista
+const expenseList = document.querySelector("ul");
 
 // Captura o evento de input para formatar o valor
 amount.oninput = () => {
@@ -21,4 +27,44 @@ function formatCurrencyBRL(value) {
   });
 
   return value;
+}
+
+// Captura o evento de submit do formulario para obter os valores
+form.onsubmit = (event) => {
+  // previne o comportamento padrao de reloado ao submit
+  event.preventDefault();
+
+  // Cria um objeto com os detalhes da nova despesa
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    created_at: new Date(),
+  };
+
+  expenseAdd(newExpense);
+};
+
+function expenseAdd(newExpense) {
+  try {
+    //cria o elemento para adicionar na lista
+    const expenseItem = document.createElement("li");
+    expenseItem.classList.add("expense");
+
+    // Cria o icone da categoria
+    const expenseIcon = document.createElement("img");
+    expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`);
+    expenseIcon.setAttribute("alt", newExpense.category_name);
+
+    // Adiciona as informações no item
+    expenseItem.append(expenseIcon);
+
+    //Adiciona o item na lista
+    expenseList.append(expenseItem);
+  } catch (error) {
+    alert("Não foi possível atualizar a lista de despesas");
+    console.log(error);
+  }
 }
